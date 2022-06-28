1fb8bbefba9fa3520cef8ee4a2ab6db7a7463c40
Ederson Torresini <boidacarapreta@gmail.com>
2022-06-28 14:39:04 -0300

    Remover botões locais, apenas entrada por serial

--- main.ts
+++ main.ts
@@ -2,8 +2,7 @@ let raspberrypi = ""
 serial.setBaudRate(BaudRate.BaudRate115200)
 let canal = 100
 radio.setGroup(canal)
-let bairro = 0
-basic.showString("" + (bairro))
+basic.showIcon(IconNames.Happy)
 basic.forever(function () {
     raspberrypi = serial.readLine()
     radio.sendString(raspberrypi)
